3e43a025c1f20f0d220595c4f613db7c4a7fde41
yuvalbl <yuval.bar.levi@gmail.com>
2019-09-25 21:35:15 +0300

    Updates

---
+++ precache-manifest.d345249a4ae13819b5c8db308939f4a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "993df6c93ecfad6093b33668b0a4168a",
+    "url": "/vision-zero/index.html"
+  },
+  {
+    "revision": "0a59032afb24c3e90edd",
+    "url": "/vision-zero/static/css/main.875b0217.chunk.css"
+  },
+  {
+    "revision": "97a8c306c71828c64d26",
+    "url": "/vision-zero/static/js/2.231f6aae.chunk.js"
+  },
+  {
+    "revision": "0a59032afb24c3e90edd",
+    "url": "/vision-zero/static/js/main.fa9cee7f.chunk.js"
+  },
+  {
+    "revision": "e6f33fe05347611c35c6",
+    "url": "/vision-zero/static/js/runtime-main.f1a350c9.js"
+  },
+  {
+    "revision": "5faec2da0379d8039d32506a261ab0f2",
+    "url": "/vision-zero/static/media/exemple1.5faec2da.png"
+  }
+]);
